c08aeeae99c8f16dd3de84a16d34f4b0376c8f81
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-06-18 16:43:31 +0100

    organization of the crudes

---
+++ src/actions/userActions.js
@@ -0,0 +1,7 @@
+import {ADD_USER, EDIT_USER, DELETE_USER} from './types';
+import Axios from 'axios';
+
+
+
+
+
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,9 @@
+import {ADD_USER, EDIT_USER, DELETE_USER} from '../actions/types';
+
+const initialState = [];
+
+export default function userReducer(state = initialState, action){
+    return state;
+}
+
+
